93fadf1bc1281fc64cac6d4a02db2b4deb2595bc
Umer Majeed <umajeed@vinsuite.com>
2024-05-30 19:52:02 +0500

    added tag for post

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -2,10 +2,11 @@
 import { notFound } from "next/navigation"
 import { getPost as getPostNotCached } from "@/lib/posts"
 import { cache } from "react"
+import Link from "next/link"
 
 const getPost = cache(async (slug) => await getPostNotCached(slug))
 
-export async function generateMetadata({params}, parent) {
+export async function generateMetadata({params}) {
   
   try {
     const {frontmatter} = await getPost(params.slug)
@@ -27,6 +28,17 @@ const BlogPage = async ({params}) => {
 
   return (
     <div className='prose dark:prose-invert'>{params.slug}
+    <article className="prose dark:prose-invert">
+      <div className="flex space-x-2 mb-8">
+        {
+          post?.frontmatter?.tags?.map(tag => (
+            <Link key={tag} href={`/blog?tags=${tag}`} className="dark:text-gray-400 text-gray-500">
+              #{tag}
+            </Link>
+          ))
+        }
+      </div>
+    </article>
       {post.content}
       {/* <MDXRemote source={markdown} /> */}
     </div>
